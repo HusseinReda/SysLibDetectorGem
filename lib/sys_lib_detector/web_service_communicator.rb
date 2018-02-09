e0673f7bc8c339312e23f4e0be9c4ac3126d587c
Hussein Abu Maash <hussein.abumaash@gmail.com>
2018-02-09 15:53:19 +0200

    documenting web_service_communicator

--- lib/sys_lib_detector/web_service_communicator.rb
+++ lib/sys_lib_detector/web_service_communicator.rb
@@ -2,9 +2,13 @@ require 'rest-client'
 require 'json'
 
 module SysLibDetector
+	# Handler for the communication with the web-service for retrieving 
+	# the required system libraries
 	class WebServiceCommunicator
+		# Url for the web-service libraries retrieval endpoint
 		SERVICE_URL = "https://sys-libraries.herokuapp.com/libraries"
 
+		# Initializing the handler object, with a check for internet connectivity
 		def initialize
 			# checking internet connectivity
 			raise Exception::NoInternetConnection if ping != :pong
@@ -12,6 +16,12 @@ module SysLibDetector
 			@url = SERVICE_URL
 		end
 
+		# Retrieving the required system libraries by sending a get request 
+		# to the web-service with the gems' names and current operating system
+		# @param gems_names The current project's gems' names
+		# @param os_name The operating system's name as a symbol (:linux, :osx, :windows)
+		# @return [Json] The web-services's response, 
+		# 	in format { gem_1: [a,b,c], gem_2: [x,y,z] }
 		def retrieve_sys_libraries(gems_names, os_name)
 			params = {
 				gems: gems_names,
@@ -21,6 +31,9 @@ module SysLibDetector
 		end
 
 		private
+
+			# Pinging google.com for checking the internet connectivity, by
+			# making a simple get request to www.google.com
 			def ping
 				begin
 					RestClient.get("https://www.google.com")
