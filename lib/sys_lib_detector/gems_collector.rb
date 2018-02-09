580eb1a869ddcfcdce66d259f9fcf319b1c2cede
Hussein Abu Maash <hussein.abumaash@gmail.com>
2018-02-09 16:29:56 +0200

    documenting system detector class

--- lib/sys_lib_detector/gems_collector.rb
+++ lib/sys_lib_detector/gems_collector.rb
@@ -1,3 +1,7 @@
+# Namespace for classes handling the whole process of the gem 
+# from collecting the projects' gem, going through system libraries retrieval, 
+# till displaying the results or installing the required system libraries
+
 module SysLibDetector
 	# Class responsible for collecting the projects' local gems
 	class GemsCollector
--- lib/sys_lib_detector/system_detector.rb
+++ lib/sys_lib_detector/system_detector.rb
@@ -1,7 +1,9 @@
 require 'system'
 
 module SysLibDetector
+	# Class responsible for getting information about the current system
 	class SystemDetector
+		# Getting the current operating system's name
 		def self.get_os_name
 			return System::OS.name
 		end
